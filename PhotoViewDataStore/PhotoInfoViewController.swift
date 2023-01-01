c768c711f35e5a3bd946af6d30c0a8d3b03df2bd
Tianbo <tianbo@hey.com>
2023-01-01 14:37:38 -0800

    image detail view

---
+++ PhotoViewDataStore/PhotoInfoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoInfoView.swift
+//  PhotoViewDataStore
+//
+//  Created by Tianbo Qiu on 1/1/23.
+//
+
+import UIKit
+
+class PhotoInfoViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var photo: Photo! {
+        didSet {
+            navigationItem.title = photo.title
+        }
+    }
+    
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchImage(for: photo) { result in
+            switch result {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error fetching image for photo: \(error)")
+            }
+        }
+    }
+}
